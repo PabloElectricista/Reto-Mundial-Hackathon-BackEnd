adb4c5ff28c6cb1c7ad83069b3f6749a239bc347
Pablo Spector <pablo_spector@hotmail.com>
2022-11-17 20:31:14 -0300

    centralización de errores y correccion en dependencia

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const auth = require('../middleware/auth');
 
-router.get('/', auth, (req, res) => res.send('Hello world!'));
+router.get('/', (req, res) => res.send('Hello world!'));
 
 router.use('/user', auth, require('./user.routes'));
 
