e0155864336cd18a6720a2eac6c6297606992df0
Pablo Spector <pablo_spector@hotmail.com>
2022-11-17 20:23:30 -0300

    centralización de errores y correccion en dependencia

--- src/app.js
+++ src/app.js
@@ -5,4 +5,14 @@ app.set('port', process.env.PORT || 3001);
 
 app.get('/', require('./routes'));
 
+app.use(async (error, req, res, next) => {
+
+    // const userdata = req.userdata;
+    // if (decoded) await errorMail(userdata.email, message)
+    
+    const { status, message } = error;
+    res.status(status || 500).send({ message })
+
+})
+
 module.exports = app;
