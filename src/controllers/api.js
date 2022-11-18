6f45de07896e7b822f4bfa79cbf37516cf819732
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2022-11-18 16:28:47 -0300

    Update api.js

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -25,7 +25,7 @@ const setVote = async (req, res, next) => {
 const getStadistics = async (req, res, next) => {
   let { limit = 8 } = req.query
   try {
-    const countries = await Countries.find().sort({ done: -1 }).limit(limit)
+    const countries = await Countries.find().sort({ done: 1 }).limit(limit)
     res.json(countries)
     // res.json(require('./temp.json'));
   } catch (error) {
