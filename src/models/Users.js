1322243ea604dc1e2fdab719eaa7221d704d55d5
Haru <frankcito639@gmail.com>
2022-11-18 08:48:30 -0500

    model Users

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,8 +1,9 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
   name: { type: String },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   nationality: { type: String, required: true },
   voted: { type: Boolean, default: false },
 });
